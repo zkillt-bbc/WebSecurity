3ad8537432f514a54b73fe28985de20a0178223e
Tim Killenberger <Tim.Killenberger@bbcag.ch>
2015-06-12 16:00:47 +0200

    First Push
    <3

---
+++ index.php
@@ -0,0 +1,37 @@
+<html>
+<head>
+<link rel="stylesheet" href="style.css">
+<title>WebSecurity</title>
+</head>
+<body>
+
+<div class="title">
+	<h1>WebSecurity</h1>
+</div>
+
+<div class="form-group code-input">
+  <h4>Eingabe</h4>
+  <div class="input-group">
+    <span class="input-group-addon">{ }</span>
+    <input type="text" class="form-control">
+    <span class="input-group-btn">
+      <button class="btn btn-primary" type="button">Senden</button>
+    </span>
+  </div>
+</div>
+
+<br>
+<br>
+<br>
+
+<div class="panel panel-primary code-output">
+  <div class="panel-heading">
+    <h3 class="panel-title">Resultat</h3>
+  </div>
+  <div class="panel-body">
+    ...
+  </div>
+</div>
+
+</body>
+</html>
